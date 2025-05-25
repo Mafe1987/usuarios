d1b78f74163caf350c9efcc4c28755154eac8029
mafe <mafeguevara87@gmail.com>
2025-05-24 19:02:20 -0500

    ultimaModificacion

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -15,9 +15,11 @@ export const Inicio = () => {
       }}>
         <h1 style={{fontSize:50, fontWeight:'grey'}}>Inicio</h1>
         
+
+        <img src="https://media.istockphoto.com/id/1953106649/es/foto/conferencia-virtual-seminario-web-pantalla-de-video-de-reuni%C3%B3n-en-l%C3%ADnea-grupo-de-empleados.webp?a=1&b=1&s=612x612&w=0&k=20&c=scDziHsY-gKg3FnMhZKe-b_Z7WCzg1U5gxPKd4QoLgs=" alt="" />
          <p style={{maxWidth: 500}}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum illo impedit velit. Esse dignissimos quisquam 
-            voluptates voluptatum incidunt vero, non ipsa nesciunt, eaque odit qui eos reiciendis omnis officia porro.
+            La Mejor app de Usuarios, made in Ecuador, por 
+            John William Lara Bello.
          </p>
          <div style={{display:'flex', gap:15}}>
          <Link to='/usuarios'
